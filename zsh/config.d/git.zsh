6f02e26f9162979a4462ec2b6456088823e87fc3
Isaac Hillegers <isaachillegers@gmail.com>
2023-12-11 14:59:32 +1100

    adding fedora scripts. Also Adding some lazy git defaults

--- zsh/config.d/git.zsh
+++ zsh/config.d/git.zsh
@@ -1,1 +1,14 @@
 # Using OhMyZsh Defaults
+
+function git_branch_full() {
+    branch=$(git symbolic-ref HEAD 2> /dev/null | awk 'BEGIN{FS="refs/heads/"} { print $NF }')
+    if [[ $branch == "" ]]; then
+        :
+    else
+        echo $branch
+    fi
+}
+
+function glazy() {
+    git add . && git commit -m "$1" && git push origin $(git_branch_full)
+}
