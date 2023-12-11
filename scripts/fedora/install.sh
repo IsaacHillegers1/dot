69c6037bdfe067602b9655de32314bf90fbfc4c8
Isaac Hillegers <isaachillegers@gmail.com>
2023-12-11 15:22:37 +1100

    further fedora updates

--- scripts/fedora/install.sh
+++ scripts/fedora/install.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
 
-sudo dnf install zsh -y
+set -x
+
+# install aws cli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip -qq
+sudo ./aws/install
+rm -rf awscliv2.zip aws
+
+# install
+# git
+# zsh
+# github cli (for authentication)
+sudo dnf install git zsh gh -y
+set +x
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+set -x
